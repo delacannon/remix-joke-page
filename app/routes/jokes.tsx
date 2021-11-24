95f352cc36d6f0869ac276e9002d499eefd0f34e
Delacannon <delacannon@gmail.com>
2021-11-24 12:34:48 +0100

    added auth

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,68 +1,87 @@
-import { LinksFunction, LoaderFunction } from "remix";
-import { Link, Outlet, useLoaderData } from "remix";
-import { db } from "~/utils/db.server";
+import { User } from "@prisma/client";
+import { Link, LinksFunction, LoaderFunction, useLoaderData } from "remix";
+import { Outlet } from "remix";
+import { db } from "../utils/db.server";
+import { getUser } from "../utils/session.server";
 import stylesUrl from "../styles/jokes.css";
 
 export let links: LinksFunction = () => {
-	return [
-		{
-			rel: "stylesheet",
-			href: stylesUrl,
-		},
-	];
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
 };
 
 type LoaderData = {
-	jokeListItems: Array<{ id: string; name: string }>;
+  user: User | null;
+  jokeListItems: Array<{ id: string; name: string }>;
 };
 
-export let loader: LoaderFunction = async () => {
-	let data: LoaderData = {
-		jokeListItems: await db.joke.findMany({
-			take: 5,
-			select: { id: true, name: true },
-			orderBy: { createdAt: "desc" },
-		}),
-	};
-	return data;
+export let loader: LoaderFunction = async ({ request }) => {
+  let jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  let user = await getUser(request);
+
+  let data: LoaderData = {
+    jokeListItems,
+    user,
+  };
+  return data;
 };
 
 export default function JokesRoute() {
-	let data = useLoaderData<LoaderData>();
+  let data = useLoaderData<LoaderData>();
 
-	return (
-		<div className="jokes-layout">
-			<header className="jokes-header">
-				<div className="container">
-					<h1 className="home-link">
-						<Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
-							<span className="logo">🤪</span>
-							<span className="logo-medium">J🤪KES</span>
-						</Link>
-					</h1>
-				</div>
-			</header>
-			<main className="jokes-main">
-				<div className="container">
-					<div className="jokes-list">
-						<Link to=".">Get a random joke</Link>
-						<p>Here are a few more jokes to check out:</p>
-						<ul>
-							{data.jokeListItems.map((joke) => (
-								<li key={joke.id}>
-									<Link to={joke.id}>{joke.name}</Link>
-								</li>
-							))}
-						</ul>
-						<Link to="new" className="button">
-							Add your own
-						</Link>
-					</div>
-					<div className="jokes-outlet">
-						<Outlet />
-					</div>
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </div>
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
